9a02ec3bba037780fc2ab6f92de16808c20efe52
Seigo Okumura <seigo.okumura@mono-wireless.com>
2022-10-04 15:12:11 +0900

    align an interface to Print::flush() to avoid compile error.

--- src/aJson/aJSON.cpp
+++ src/aJson/aJSON.cpp
@@ -510,7 +510,7 @@ aJsonStream::skip()
 // Utility to flush our buffer in case it contains garbage
 // since the parser will return the buffer untouched if it
 // cannot understand it.
-int
+void
 aJsonStream::flush()
 {
   int in = this->getch();
@@ -518,9 +518,7 @@ aJsonStream::flush()
   {
     in = this->getch();
   }
-  return EOF;
 }
-
 
 // Parse an object - create a new root, and populate.
 aJsonObject*
